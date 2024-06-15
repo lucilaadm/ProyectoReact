d69c1a3b4943e4177f026a117d5ab7c71c9324e6
amanddemendietalucila <amanddemendietalucila@gmail.com>
2024-06-15 10:31:23 +0000

    agrego ejercicio 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+
 import HolaMundo from "./components/pages/Ejercicio1";
 import Ejercicio2 from "./components/pages/Ejercicio2";
 import Ejercicio3 from "./components/pages/Ejercicio3";
+import Ejercicio4 from "./components/pages/Ejercicio4";
 
 const App = () => {
     return (
@@ -8,6 +10,7 @@ const App = () => {
         <HolaMundo/>
         <Ejercicio2 mensaje="mi amigo!"/>
         <Ejercicio3 mensaje="mi amigo!"/>
+        <Ejercicio4 />
       </>
     );
   };
---
+++ src/components/pages/Ejercicio4.jsx
@@ -0,0 +1,38 @@
+
+import { useRef, useState } from "react";
+
+const Ejercicio4 = () => {
+    const [todos, setTodos] = useState (["Hacer ejercicios", "Mirar clases"])
+    
+    const $inputRef = useRef();
+
+    const handleNuevaTarea = () => {
+      const nuevaTarea = $inputRef.current.value;
+      const nuevoArreglo = [nuevaTarea, ...todos]
+      setTodos(nuevoArreglo);
+
+      $inputRef.current.value = "";
+    }
+
+  return (
+    <section>
+      {/*creacion de tareas*/}
+        <h1>Bienvenido</h1>
+        <p>Ingresa tus tareas:</p>
+        <div>
+          <input type="text" ref={$inputRef} />
+        </div>
+        <button onClick={handleNuevaTarea} className='btn btn-primary mt-3' >Agregue tarea</button>
+      {/*listado de tareas*/}
+      <ul>
+        {todos.map((tarea, index) => (
+          <li key={index}>{tarea}</li>
+            )
+          )
+        }
+      </ul>
+    </section>
+    );
+  };
+
+export default Ejercicio4;
